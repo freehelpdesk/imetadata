ed331d95225d72971add92781636480547a80096
freehelpdesk <freehelpdesk@proton.me>
2024-05-14 10:42:12 -0400

    metadata: add log & fix icon edge case

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use std::{
     path::PathBuf,
 };
 use zip::ZipArchive;
+use tracing::*;
 
 mod api;
 
@@ -34,6 +35,7 @@ struct Info {
     c_f_bundle_name: Option<String>,
     c_f_bundle_icon_files: Option<Vec<String>>,
     c_f_bundle_icons: Option<CFBundleIcons>,
+    c_f_bundle_short_version_string: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -46,7 +48,7 @@ struct CFBundleIcons {
 #[serde(rename_all = "PascalCase")]
 struct CFBundlePrimaryIcon {
     c_f_bundle_icon_files: Vec<String>,
-    c_f_bundle_icon_name: String,
+    c_f_bundle_icon_name: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -65,6 +67,10 @@ async fn main() {
     let cli = Cli::parse();
     let api = api::Api::new("us");
 
+    tracing_subscriber::fmt()
+        .with_env_filter("imetadata=trace")
+        .init();
+
     let mut ipas: Vec<PathBuf> = vec![];
 
     for input in cli.input {
@@ -76,18 +82,18 @@ async fn main() {
         if input.is_dir() {
             // we need to go lookin now
             let Ok(paths) = fs::read_dir(input) else {
-                println!("Not a directory, how did we get here?");
+                error!("Not a directory, how did we get here?");
                 return;
             };
             for path in paths {
                 let Ok(entry) = path else {
-                    println!("Error in entry.");
+                    error!("Error in entry.");
                     return;
                 };
                 //if cli.debug > 0 { println!("{}", &entry.path().to_string_lossy()) }
                 if let Some(extension) = entry.path().extension() {
                     if extension.to_string_lossy() == "ipa" {
-                        println!("{} is ipa", entry.path().to_string_lossy());
+                        info!("{} is ipa", entry.path().to_string_lossy());
                         ipas.push(entry.path())
                     }
                 }
@@ -98,17 +104,17 @@ async fn main() {
 
         for ipa in &ipas {
             let Ok(file_archive) = File::open(ipa) else {
-                println!("Failed to open IPA file :(");
+                error!("Failed to open IPA file :(");
                 return;
             };
 
             let Ok(mut archive) = ZipArchive::new(&file_archive) else {
-                println!("is not an actual ipa lol");
+                error!("is not an actual ipa lol");
                 return;
             };
 
-            let Ok(mut second_archive) = ZipArchive::new(&file_archive) else {
-                println!("is not an actual ipa lol");
+            let Ok(mut archive_dup) = ZipArchive::new(&file_archive) else {
+                error!("is not an actual ipa lol");
                 return;
             };
 
@@ -122,24 +128,26 @@ async fn main() {
                     if name.file_name() == Some(OsStr::new("Info.plist"))
                         && name.components().count() == 3
                     {
-                        println!("{} plist found :P", name.to_string_lossy());
+                        info!("{} found", name.to_string_lossy());
                         let mut buf = Vec::with_capacity(entry.size() as usize);
                         entry.read_to_end(&mut buf).unwrap();
                         //println!("{}", std::str::from_utf8(&buf).unwrap());
                         let info: Info = plist::from_bytes(&buf).unwrap();
-                        println!("App Display Name: {:?}", info.c_f_bundle_display_name);
-                        println!("App Name: {:?}", info.c_f_bundle_name);
-                        println!("App Identifier: {}", info.c_f_bundle_identifier);
+
+                        info!("[{}] {} {}", &info.c_f_bundle_identifier, info.c_f_bundle_display_name.as_ref().unwrap_or(&info.c_f_bundle_name.clone().unwrap_or("N/A".to_string())), &info.c_f_bundle_short_version_string);
 
                         let name = if let Ok(e) = api.lookup(&info.c_f_bundle_identifier).await {
-                            println!("App Artist: {}", e.artist_name);
+                            info!("Developer: {}", e.artist_name);
                             Some(e.artist_name)
                         } else {
-                            println!("Failed to lookup app info :(");
+                            warn!("Appstore info not found for {}, not adding a developer.", &info.c_f_bundle_identifier);
                             None
                         };
 
+                        // let name = None;
+
                         let mut icons: Vec<String> = vec![];
+
                         let mut icon_file_list: Vec<String> = vec![];
 
                         // far out, now we have to find all the fucking icons somehow lmao
@@ -156,14 +164,15 @@ async fn main() {
 
                         let mut modify = cli.output.clone();
                         modify.push(info.c_f_bundle_identifier.clone());
+
                         fs::create_dir_all(&modify).unwrap();
-                        for j in 0..second_archive.len() {
-                            let mut entry = second_archive.by_index(j).unwrap();
+                        for j in 0..archive_dup.len() {
+                            let mut entry = archive_dup.by_index(j).unwrap();
                             let name = entry.enclosed_name();
-                            if let Some(name) = name {
-                                let name = name.file_name().unwrap().to_string_lossy().to_string();
+                            if let Some(path) = name {
+                                let name = path.file_name().unwrap().to_string_lossy().to_string();
                                 for icon in &icons {
-                                    if name.starts_with(icon) && name.ends_with(".png") {
+                                    if name.starts_with(icon) && (path.extension().unwrap().to_string_lossy() == "png") {
                                         let mut buf = Vec::with_capacity(entry.size() as usize);
                                         entry.read_to_end(&mut buf).unwrap();
                                         let mut name_buf = modify.clone();
